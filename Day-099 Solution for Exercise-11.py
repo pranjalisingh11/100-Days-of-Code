693f361357a17e1973954062f4718a87bea8f14c
pranjalisingh11 <125385423+pranjalisingh11@users.noreply.github.com>
2025-08-27 22:45:41 +0530

    Add files via upload

---
+++ Day-099 Solution for Exercise-11.py
@@ -0,0 +1,26 @@
+import time
+import winsound
+from win10toast import ToastNotifier
+import win32com.client
+
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+toaster = ToastNotifier()
+interval = 5
+
+while True:
+    winsound.Beep(1000, 1000)
+    # First argument 1000 → the frequency(pitch of sound) in Hertz(Hz). 1000 Hz is like a medium beep.
+    # Second argument 1000 -> the duration of the beep in milliseconds. 1000 ms = 1 second.
+    # So: "Beep at 1000 Hz for 1 second."
+    toaster.show_toast(
+        "💧 Water Reminder",
+        "Hey Gungun, Drink water now!",
+        duration=10,
+        threaded=True  # This makes it non-blocking
+    )
+    # "title", "message body of the notification", "the notification stays on screen for 10 seconds".
+
+    speaker.Speak("Hey Gungun, drink water now!")  # Speaks while popup is visible
+
+    time.sleep(interval)
